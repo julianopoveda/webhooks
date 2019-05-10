83dd03d951aa863c8ed009167978a7ddc0a95c61
Poveda <julianopoveda@bitbucket.org>
2019-05-09 21:37:11 -0300

    Implementado o endpoint que cálcula o número primo da posicao informada

---
+++ Api/Controllers/PrimesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrimesController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        ///<summary>
+        /// Calcula o número primo na posição informada
+        /// <param name="primeIndex">Posição do primo desejado</param>
+        /// <returns>o número primo calculado</returns>
+        ///<summary>
+        public int Post([FromBody] int primeIndex)
+        {
+            if (primeIndex < 1)
+            {
+                Response.StatusCode = 400;
+                return -1;
+            }
+
+            if (primeIndex == 1)
+                return 2;
+            else
+            {
+                try
+                {
+                    return CalculatePrime(primeIndex);
+                }
+                catch (Exception e)
+                {
+                    //TODO: log error
+                    Response.StatusCode = 400;
+                    return -1;
+                }
+                finally
+                {
+                    //TODO: do someting
+                }
+            }
+        }
+
+        private int CalculatePrime(int primeIndex)
+        {
+            int prime = 1;
+            try
+            {
+                for (int i = 1, j = 1; j < primeIndex; i++)
+                {
+                    int primeCandidate = 2 * i + 1;
+                    int squareRoot = (int)Math.Floor(Math.Sqrt(primeCandidate));
+                    for (int divisonAttemp = 0; divisonAttemp < squareRoot; divisonAttemp++)
+                    {
+                        if (primeCandidate % divisonAttemp != 0)
+                            prime = primeCandidate;
+                        else
+                            break;//Caso exista um divisor o número não é primo e não há motivos para seguir no teste.
+                    }
+
+                    if (prime == primeCandidate)
+                        j++;
+                }
+                return prime;
+            }
+            catch (Exception e)
+            {
+                //TODO: log error
+                throw;
+            }
+            finally
+            {
+                //TODO: log action                
+            }
+        }
+    }
+}
