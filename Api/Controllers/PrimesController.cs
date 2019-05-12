9c4d199cfc870a2251633fbe0593a08f5a5f4b85
Juliano Poveda <julianopoveda@gmail.com>
2019-05-11 21:41:29 -0300

    Cálculo do primo movido para a model Prime como um método. Retirada a responsabiliade do método post de pré calcular o primo.

--- Api/Controllers/PrimesController.cs
+++ Api/Controllers/PrimesController.cs
@@ -22,60 +22,26 @@ namespace Api.Controllers
         /// <returns>o número primo calculado</returns>
         ///<summary>
         public int Post([FromBody] Prime prime)
-        {            
-            if (prime.primeIndex < 1)
+        {
+            try
             {
+                int calculatedPrime = prime.CalculatePrime(_eventId, _logger);
+
+                if (calculatedPrime < 1)
+                    Response.StatusCode = 400;
+
+                return calculatedPrime;
+            }
+            catch (Exception e)
+            {
+                _logger.LogDebug(_eventId, e, "Ocorreu um erro ao calcular o primo de posicao {0}", prime.PrimeIndex);
+
                 Response.StatusCode = 400;
                 return -1;
             }
-
-            if (prime.primeIndex == 1)
-                return 2;
-            else
-            {
-                try
-                {
-                    return CalculatePrime(prime.primeIndex);
-                }
-                catch (Exception e)
-                {
-                    _logger.LogDebug(_eventId, e, "Ocorreu um erro ao calcular o primo de posicao {0}", prime.primeIndex);
-
-                    Response.StatusCode = 400;
-                    return -1;
-                }
-                finally
-                {
-                    _logger.LogInformation(_eventId, "Request para PrimeController método Post efetuado. Retornou status {0}", Response.StatusCode);
-                }
-            }
-        }
-
-        private int CalculatePrime(int primeIndex)
-        {
-            int prime = 1;
-            try
-            {
-                for (int i = 1, j = 1; j < primeIndex; i++)
-                {
-                    int primeCandidate = 2 * i + 1;
-                    int squareRoot = (int)Math.Floor(Math.Sqrt(primeCandidate));
-                    for (int divisonAttemp = 0; divisonAttemp < squareRoot; divisonAttemp++)
-                    {
-                        if (primeCandidate % divisonAttemp != 0)
-                            prime = primeCandidate;
-                        else
-                            break;//Caso exista um divisor o número não é primo e não há motivos para seguir no teste.
-                    }
-
-                    if (prime == primeCandidate)
-                        j++;
-                }
-                return prime;
-            }
             finally
             {
-                _logger.LogInformation(_eventId, "Finalizando método CalculatePrime com o primo {0}", prime);                
+                _logger.LogInformation(_eventId, "Request para PrimeController método Post efetuado. Retornou status {0}", Response.StatusCode);
             }
         }
 
--- Api/Models/Prime.cs
+++ Api/Models/Prime.cs
@@ -1,7 +1,52 @@
+using System;
+using Microsoft.Extensions.Logging;
+
 namespace Api.Models
 {
     public class Prime
     {
-        public int primeIndex { get; set; }
+        public int PrimeIndex { get; set; }
+
+        //
+        /// <summary>
+        /// Calcula o primo na posicao informada na propriedade PrimeIndex
+        /// </summary>
+        /// <param name="eventId">id do evento de log atual</param>
+        /// <param name="logger">estrutura de log para possíveis logs de execução</param>
+        /// <returns>O primo na posição solicitada</returns>
+        public int CalculatePrime(EventId eventId, ILogger logger)
+        {
+            if (PrimeIndex < 1)
+                return -1;
+            else if (PrimeIndex == 1)
+                return 2;
+            else
+            {
+                int prime = 1;
+                try
+                {
+                    for (int i = 1, j = 1; j < PrimeIndex; i++)
+                    {
+                        int primeCandidate = 2 * i + 1;
+                        int squareRoot = (int)Math.Floor(Math.Sqrt(primeCandidate));
+                        for (int divisonAttemp = 0; divisonAttemp < squareRoot; divisonAttemp++)
+                        {
+                            if (primeCandidate % divisonAttemp != 0)
+                                prime = primeCandidate;
+                            else
+                                break;//Caso exista um divisor o número não é primo e não há motivos para seguir no teste.
+                        }
+
+                        if (prime == primeCandidate)
+                            j++;
+                    }
+                    return prime;
+                }
+                finally
+                {
+                    logger.LogInformation(eventId, "Finalizando método CalculatePrime com o primo {0}", prime);
+                }
+            }
+        }
     }
 }
